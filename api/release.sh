014e77cedfd8b75497cdfab7ff128b1034f1a336
Eren <erenyeager@gmail.com>
2024-05-14 09:44:56 +0700

    Deploy file

---
+++ api/release.sh
@@ -0,0 +1,2 @@
+docker compose -p p-auto-number-api down --rmi all
+docker compose -p p-auto-number-api up -d
---
+++ fe/release.sh
@@ -0,0 +1,2 @@
+docker compose -p p-auto-number-fe down --rmi all
+docker compose -p p-auto-number-fe up -d
