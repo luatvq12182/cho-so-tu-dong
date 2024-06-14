9880808e9a02f5244c64488da67b3d9d2c7f939f
Eren <erenyeager@gmail.com>
2024-06-14 08:36:08 +0700

    await reset cache

--- api/schedule.js
+++ api/schedule.js
@@ -5,30 +5,30 @@ const SoiCauHangNgayService = require('./src/services/SoiCauHangNgay');
 const logger = require('./src/configs/logger');
 const { cache } = require("./src/configs/cache");
 
-schedule.scheduleJob("0 1 * * *", function () {
+schedule.scheduleJob("0 1 * * *", async function () {
     logger.info('BẮT ĐẦU CHO SỐ');
-    SoHangNgayService.autoGenNumbers();
-    LoTopService.autoGenNumbers();
-    SoiCauHangNgayService.autoGenNumbers();
+    await SoHangNgayService.autoGenNumbers();
+    await LoTopService.autoGenNumbers();
+    await SoiCauHangNgayService.autoGenNumbers();
     cache.reset();
 });
 
-schedule.scheduleJob("35 18 * * *", function () {
+schedule.scheduleJob("35 18 * * *", async function () {
     logger.info('BẮT ĐẦU CHECK KẾT QUẢ');
-    SoHangNgayService.checkResult();
-    LoTopService.checkResult();
-    SoiCauHangNgayService.checkResult(1);
+    await SoHangNgayService.checkResult();
+    await LoTopService.checkResult();
+    await SoiCauHangNgayService.checkResult(1);
     cache.reset();
 });
 
-schedule.scheduleJob("45 16 * * *", function () {
+schedule.scheduleJob("45 16 * * *", async function () {
     logger.info('BẮT ĐẦU CHECK KẾT QUẢ');
-    SoiCauHangNgayService.checkResult(3);
+    await SoiCauHangNgayService.checkResult(3);
     cache.reset();
 });
 
-schedule.scheduleJob("45 17 * * *", function () {
+schedule.scheduleJob("45 17 * * *", async function () {
     logger.info('BẮT ĐẦU CHECK KẾT QUẢ');
-    SoiCauHangNgayService.checkResult(2);
+    await SoiCauHangNgayService.checkResult(2);
     cache.reset();
 });
